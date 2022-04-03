2565a1a4ee914267fdf055138213f143ec45d58e
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-04-03 22:15:59 +0300

    Config preset

---
+++ Config.php
@@ -0,0 +1,13 @@
+<?php
+
+class Config {
+    public const DB_URL = '127.0.0.1';
+    public const DB_USER = 'lambda_dog';
+    public const DB = 'lambda_dog';
+    public const DB_PASS = 'samplepassword';
+
+    public const SMTP_SERV = 'smtp.mail.ru';
+    public const SMTP_USR = 'demo@mail.ru';
+    public const SMTP_USRNAME = 'Demo User';
+    public const SMTP_PWD = '12345'; // "External password" for some resources
+}
