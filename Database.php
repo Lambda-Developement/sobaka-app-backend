a81240fe2ed757f01ae84bf8de3359827487ed89
Duangellow <36920878+DuangellowX@users.noreply.github.com>
2022-04-07 14:40:59 +0300

    fastPrepare method

--- Database.php
+++ Database.php
@@ -8,43 +8,37 @@ class Database extends mysqli implements DatabaseInterface {
         parent::__construct(Config::DB_URL, Config::DB_USER, Config::DB_PASS, Config::DB)
             or throw new DatabaseException("Unable to connect!");
     }
-    public function getUserByLogin(string $login): array {
-        $prep = self::prepare("SELECT * FROM users WHERE login = ?");
-        $prep->bind_param('s', $login);
+    private function fastPrepare(string $query, string $types, &$var1, &...$vars): mysqli_result {
+        $prep = self::prepare($query);
+        $prep->bind_param($types, $var1, $vars);
         $prep->execute();
-        $f = $prep->get_result();
+        $r = $prep->get_result();
         $prep->close();
+        return $r;
+    }
+    public function getUserByLogin(string $login): array {
+        $f = self::fastPrepare("SELECT * FROM users WHERE login = ?", 's', $login);
         if ($f->num_rows != 1) throw new DatabaseException("User does not exists!");
         return $f->fetch_assoc();
     }
     public function getUserByKey(string $key): array {
-        $prep = self::prepare("SELECT * FROM users WHERE loginkey = ?");
-        $prep->bind_param('s', $key);
-        $prep->execute();
-        $f = $prep->get_result();
+        $f = self::fastPrepare("SELECT * FROM users WHERE loginkey = ?", 's', $key);
         if ($f->num_rows != 1) throw new DatabaseException("User does not exists!");
         return $f->fetch_assoc();
     }
     public function getLoginKeyUsage(string $key): int {
-        $prep = self::prepare("SELECT id FROM users WHERE loginkey = ?");
-        $prep->bind_param('s', $key);
-        $prep->execute();
-        return $prep->get_result()->num_rows;
+        return self::fastPrepare("SELECT id FROM users WHERE loginkey = ?", 's', $key)->num_rows;
     }
     public function assignKeyToUserID(string $key, int $user_id): void {
-        $prep = self::prepare("UPDATE users SET loginkey = ? WHERE id = ?");
-        $prep->bind_param('ss', $key, $user_id);
-        $prep->execute();
+        self::fastPrepare("UPDATE users SET loginkey = ? WHERE id = ?", 'ss', $key, $user_id);
     }
     public function insertUser(string $email, string $name, string $hash): void {
         try {
             $this->getUserByLogin($email);
             throw new UserAlreadyRegisteredException();
         } catch (DatabaseException $e) {
-            $prep = self::prepare("INSERT INTO users(login, name, hash) VALUES (?, ?, ?)");
-            $prep->bind_param('sss', $email, $name, $hash);
-            $prep->execute() or die($prep->error);
-            $prep->close();
+            self::fastPrepare("INSERT INTO users(login, name, hash) VALUES (?, ?, ?)", 'sss', $email, $name, $hash);
+        }
         }
     }
     public function setUserPassword(string $email, string $hash): void {
@@ -53,30 +47,20 @@ class Database extends mysqli implements DatabaseInterface {
         } catch (DatabaseException $e) {
             throw new UserNotFoundException(previous: $e);
         }
-        $prep = self::prepare("UPDATE users SET hash = ? WHERE login = ?");
-        $prep->bind_param('ss', $hash, $email);
-        $prep->execute();
-        $prep->close();
+        self::fastPrepare("UPDATE users SET hash = ? WHERE login = ?", 'ss', $hash, $email);
     }
     public function getData(): array {
         $q = self::query("SELECT lat, lon, description FROM points");
         return $q->fetch_all();
     }
     public function getSources(int $tour_id): array {
-        $prep = self::prepare("SELECT audio, subtitles FROM points WHERE id = ?");
-        $prep->bind_param('i', $tour_id);
-        $prep->execute();
-        $r = $prep->get_result();
-        $prep->close();
+        $r = self::fastPrepare("SELECT audio, subtitles FROM points WHERE id = ?", 'i', $tour_id);
         if ($r->num_rows == 0) throw new ElementNotFoundException();
         return $r->fetch_array(MYSQLI_NUM);
     }
     public function insertErrorMessage(string $message, User $sender): void {
         $senderid = $sender->id;
-        $prep = self::prepare("INSERT INTO error_messages(senderid, text) VALUES (?, ?)");
-        $prep->bind_param('is', $senderid, $message);
-        $prep->execute();
-        $prep->close();
+        self::fastPrepare("INSERT INTO error_messages(senderid, text) VALUES (?, ?)", 'is', $senderid, $message);
     }
     function __destruct() {
         self::close();
